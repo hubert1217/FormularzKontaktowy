e2bc2dddb74266d163733667700d99c13897a406
hubert1217 <hubert.roj@gmail.com>
2021-03-03 14:25:34 +0100

    utworzenie modelu

---
+++ FormularzKontaktowy/Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormularzKontaktowy.Models
+{
+    public class Client
+    {
+        public string Name { set; get; }
+        public string Email { set; get; }
+        public string Address { set; get; }
+        public string Phone { set; get; }
+        public bool? Urgent { set; get; }
+    }
+}
